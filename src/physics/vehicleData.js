0c53b96a7ca0dc2ccc7c4bbe716bd9f26652d7cc
imr4nkh8n <imr4nkh8n@gmail.com>
2025-12-29 10:24:13 +0000

    Update vehicleData.js

--- src/physics/vehicleData.js
+++ src/physics/vehicleData.js
@@ -1,1 +1,32 @@
 // vehicle specs
+export const vehicles = {
+  fuso: {
+    name: "Fuso FI 1217R",
+    payloadTons: 12,
+    extraPayloadTons: 2,
+    engineTorqueNm: 520,
+    enginePowerKw: 125,
+    drivetrainEfficiency: 0.9,
+    gearRatios: [5.8, 3.1, 1.9, 1.3, 1.0, 0.8],
+    finalDrive: 4.875,
+    tyreRadiusM: 0.48,
+    idleRpm: 800,
+    redlineRpm: 2600,
+    fuelRateGPerKwh: 215
+  },
+
+  isuzu: {
+    name: "Isuzu FRR90N",
+    payloadTons: 10,
+    extraPayloadTons: 0,
+    engineTorqueNm: 460,
+    enginePowerKw: 114,
+    drivetrainEfficiency: 0.88,
+    gearRatios: [6.0, 3.4, 2.1, 1.4, 1.0, 0.85],
+    finalDrive: 4.625,
+    tyreRadiusM: 0.47,
+    idleRpm: 800,
+    redlineRpm: 2500,
+    fuelRateGPerKwh: 225
+  }
+};
